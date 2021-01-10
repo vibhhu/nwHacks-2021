c9a181abe4af31c5b718853256b4ec0e253b2c95
pshah <pshah5000@gmail.com>
2021-01-10 04:44:05 -0800

    Fixed app.yaml file

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = require("./index.js");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://username:nwhacks2021@cluster0.ckd2z.mongodb.net/nwhacks?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", (error) => logger(error));
 db.once("open", () => logger("Database connected"));
