44c1554bc4e872b0bea9dae3df7f3f385c6a0712
pshah <pshah5000@gmail.com>
2021-01-10 07:45:21 -0800

    Added env variable for PORT

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ const express = require("express");
 const app = require("./index.js");
 const mongoose = require("mongoose");
 
+const port = process.env.PORT || 8080;
+
 mongoose.connect("mongodb+srv://username:nwhacks2021@cluster0.ckd2z.mongodb.net/nwhacks?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", (error) => logger(error));
 db.once("open", () => logger("Database connected"));
 
-app.listen(8000, () => logger("Server started"));
+app.listen(port, () => logger("Server started"));
