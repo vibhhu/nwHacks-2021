92d866927330b6f55beb8fb83fc5ea3a1b72cf34
pshah <pshah5000@gmail.com>
2021-01-10 01:18:32 -0800

    maybe fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = require("./index.js");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", (error) => logger(error));
 db.once("open", () => logger("Database connected"));
